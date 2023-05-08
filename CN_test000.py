d8887f0bc72e5a2db2a030def8e38521e571c50e
Willian <willianbarelac@gmail.com>
2023-05-08 05:07:24 -0300

    Add files via upload

---
+++ CN_test000.py
@@ -0,0 +1,141 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import teneto
+import conversion_equation
+import matplotlib.patches as patches
+from matplotlib.patches import FancyArrowPatch
+
+# Corrigir visualização usando pyvis
+
+
+#Data loading 
+gene= ['BLC2']
+rna=['miRNA1']
+protein=[]
+effect=['Apoptosis','Myogenesis']
+tissue={
+    0:['heart',''],
+    1:['penis','']}
+local= []   
+drug = []
+
+
+
+
+
+    
+#nodes universal defaults456
+node_colors = {0: 'red', #gene
+               1: 'orange', #rna
+               2: 'yellow', #protein
+               3: 'green', #effect
+               4: 'yellow', #tissue
+               5: 'aqua', #local
+               6: 'black'} #drug
+
+#edges universal defaults
+styles_produc= "-|>, head_length=0.4, head_width=0.2"
+styles_coproduc= ["<->"] 
+styles_inib= ["|-|, widthA=0, angleA=0, widthB=1.0, angleB=0",'r']
+styles_coinib= ["|-|", 'r']
+
+
+#Control construct
+tissue[1][1] = nx.DiGraph()
+
+
+#------------------
+
+#Graph point construct (aging heart)
+
+#data edges
+
+
+tissue[0][1] = nx.DiGraph()
+
+
+
+#nodes creation
+
+ #automation nodes creation
+
+tissue[0][1].add_nodes_from(gene + rna + protein + effect)
+
+ 
+#edges settings in aging momentum
+aging_edges_configuration= [(rna[0],gene[0]),(gene[0],effect[0])]
+tissue[0][1].add_edges_from(aging_edges_configuration)
+
+
+#reactions
+
+C1, C2 = conversion_equation.c_e(1.0,1.0,0.1,-0.05,2,1)
+print(C1)
+print(C2)
+
+edge_widths = {0:C1[1], 1:C2[1]} #equations results
+
+
+
+
+
+#Draws construct edges
+
+
+pos = nx.spring_layout(tissue[0][1])
+
+
+start_pos = pos[rna[0]]
+end_pos = pos[gene[0]]
+
+arrow = FancyArrowPatch(start_pos, end_pos, arrowstyle='|-|, widthA=0, angleA=0, widthB=1.0, angleB=0', color='red', mutation_scale=20)
+plt.gca().add_patch(arrow)
+
+
+##nx.draw_networkx_edges(tissue[0][1], pos, edgelist=tissue[0][1].edges([rna[0],gene[0]]),
+##                       edge_color=styles_inib[1], arrows=True, width=edge_widths[0],
+##                       arrowstyle=(styles_inib[0]))
+
+
+start_pos = pos[gene[0]]
+end_pos = pos[effect[0]]
+
+arrow = FancyArrowPatch(start_pos, end_pos, arrowstyle='->', color='blue', mutation_scale=20)
+plt.gca().add_patch(arrow)
+
+
+##nx.draw_networkx_edges(tissue[0][1], pos, edgelist=tissue[0][1].edges([gene[0],effect[0]]),
+##                       edge_color='blue', arrows=True, width=edge_widths[1],
+##                       arrowstyle=(styles_produc))
+
+
+# Draws construct nodes
+try:
+    nx.draw_networkx_nodes(tissue[0][1], pos,
+                       nodelist=[gene[i] for i in range(0,len(gene))], node_color=(node_colors[0]))
+except Exception as e:
+        pass
+try:    
+    nx.draw_networkx_nodes(tissue[0][1], pos,
+                       nodelist=[rna[i] for i in range(0,len(rna))], node_color=(node_colors[1]))
+except Exception as e:
+        pass
+
+try:
+    nx.draw_networkx_nodes(tissue[0][1], pos,
+                       nodelist=[protein[i] for i in range(0,len(protein))], node_color=(node_colors[2]))
+except Exception as e:
+        pass
+
+try:    
+    nx.draw_networkx_nodes(tissue[0][1], pos,
+                       nodelist=[effect[i] for i in range(0,len(effect))], node_color=(node_colors[3]))
+except Exception as e:
+        pass
+
+    
+nx.draw_networkx_labels(tissue[0][1], pos, font_size=9, font_family='Arial')
+
+plt.axis('off')
+plt.show() 
---
+++ conversion_equation.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#corrigir var_time
+
+def c_e(C1_0,C2_0,k1,k2,n_analyzes,var_time): 
+    """
+
+    Definitions:
+
+    C = Concetrations of agents in reactiion
+    
+    k = rate of converion (its needed to calculate for your own [yet]) 
+
+    if production the value is POSITIVE (+) if its inhibition the value is
+    NEGATIVE (-)
+
+    n_analyzes = How many times was data collection about agent behaviour
+
+    var_time= what was the time periods of data collection
+    IMPORTANT: var_time = delta_t 
+
+    """
+
+    ##Future improvements
+    #
+    # k calculation is necessary to automate
+    #
+    #-----------------------------------------
+
+
+    ## Time setting
+
+    delta_t = var_time
+    t = np.arange(0, n_analyzes, delta_t) # Time Array
+
+
+
+    # Arrays for C1 and C2
+    C1 = np.zeros_like(t)
+    C2 = np.zeros_like(t)
+
+
+    # Inicial condition
+    C1[0] = C1_0
+    C2[0] = C2_0
+
+    #-----------------------------------------------
+
+
+
+    # Functions of concentrations derivatives
+    def dC1dt(C1, C2, k1):
+        return k1 * C1 * C2
+
+    def dC2dt(C1, C2, k2):
+        return k2 * C1 * C2
+
+
+
+
+
+    # calculating for each time (the magic of derivative)
+    for i in range(1, len(t)):
+        dC1 = dC1dt(C1[i-1], C2[i-1], k1) * delta_t
+        dC2 = dC2dt(C1[i-1], C2[i-1], k2) * delta_t
+        C1[i] = C1[i-1] + dC1
+        C2[i] = C2[i-1] + dC2
+
+
+    return C1, C2
+
+    
+
+### For Plot information about concentration behavior
+
+def plot_concentrations(C1,C2,name1,name2):
+
+    """
+
+    name1= string, agent name 1
+    name2= string, agent name 2
+    
+
+    """
+ 
+    plt.plot(t, C1, label=name1)
+    plt.plot(t, C2, label=name2)
+    plt.xlabel('Time')
+    plt.ylabel('concentration')
+    plt.legend()
+    plt.show()
